542f0b3815ca3c176a0e3e93912569c105fb1e11
Gss-Hitesh <81021603+Gss-Hitesh@users.noreply.github.com>
2023-06-13 11:46:03 +0530

    docs : Find the month in the year 2016 with the highest total sales

--- question29.sql
+++ question29.sql
@@ -1,9 +1,12 @@
 /* 29. Find the month in the year 2016 with the highest total sales */
-select orders.order_date
-from (select  order_details.order_id as orddID, round(sum(order_details.unit_price * order_details.quantity - order_details.unit_price * order_details.quantity * order_details.discount),2) as summation
-from order_details
-group by order_details.order_id )
-inner join orders
-on orders.order_id = orddID 
-group by orders.order_date
-order by Round(sum(summation),2) desc limit 1 ;
+select Month(oDate) As month, round(sum(p.unit_price * od.quantity - p.unit_price * od.quantity * od.discount),2) as summation
+from products p 
+Inner join order_details od
+on p.product_id = od.product_id
+inner join
+(select o.order_id as oID, o.order_date as oDate
+ from orders o
+ where Year(o.order_date) = 2016 )
+ on od.order_id = oID 
+ Group by Month(oDate) 
+ order By round(sum(p.unit_price * od.quantity - p.unit_price * od.quantity * od.discount),2) desc limit 1;
