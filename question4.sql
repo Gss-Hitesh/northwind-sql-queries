68a16ed9b27c00ea4cda72370c6b5684ac797c7b
Gss-Hitesh <81021603+Gss-Hitesh@users.noreply.github.com>
2023-06-13 11:48:36 +0530

    docs : Find the total sales (Quantity*Unit_Price) for each category of products

--- question4.sql
+++ question4.sql
@@ -1,8 +1,7 @@
 /* 4. Find the total sales (Quantity*Unit_Price) for each category of products */
-select  categories.category_name, round(sum(order_details.quantity * products.unit_price),2) As TotalSales
-from products
-inner join order_details
-on products.product_id = order_details.product_id
-inner join categories
-on products.category_id = categories.category_id
-group by categories.category_id ;
+select c.category_name, sum(p.unit_price * o.quantity ) As "total Sales"
+from products p inner join order_details o     
+On p.product_id = o.product_id
+Inner join categories c 
+On p.category_id = c.category_id
+group by c.category_id;
