e2166846cbed023b872b1c61001e46f8674172f1
Gss-Hitesh <81021603+Gss-Hitesh@users.noreply.github.com>
2023-06-13 11:50:00 +0530

    docs : Find the total revenue for the year 2016

--- question15.sql
+++ question15.sql
@@ -1,15 +1,8 @@
 /* 15. Find the total revenue for the year 2016 */
-select sum(summation)
-from orders
-inner join (select
-  order_id as orddID,
-  round(
-    sum(
-      unit_price * quantity - (unit_price * quantity * discount)
-    ),
-    2
-  ) as summation
-from order_details
-group by orddID)
-on orders.order_id = orddID
-where shipped_date between '2016-01-01' and '2016-12-31' ;
+select round (sum(unit_price * quantity - (unit_price * quantity * discount)),2) AS "Revenue" 
+from orders o 
+inner join order_details o_d
+On o.order_id = o_d.order_id
+inner join products p
+On p.product_id=o_d.product_id
+where o.order_date between '2016-01-01' AND '2016-12-31';
