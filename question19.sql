e3f2b7e3ad72598dedb3117f77961fa7f2eeba77
Gss-Hitesh <81021603+Gss-Hitesh@users.noreply.github.com>
2023-06-13 11:52:06 +0530

    docs : Find the customers who have spent more than $5000 in total

--- question19.sql
+++ question19.sql
@@ -1,49 +1,11 @@
 /* 19. Find the customers who have spent more than $5000 in total */
-select
-  customers.contact_name,
-  summation
-from (
-    select
-      orddId,
-      summation,
-      orders.customer_id as cid
-    from (
-        select
-          order_id as orddID,
-          round(
-            sum(
-              unit_price * quantity - (unit_price * quantity * discount)
-            ),
-            2
-          ) as summation
-        from order_details
-        group by orddID
-      )
-      inner join orders on orders.order_id = orddID
-  )
-  inner join customers on cid = customers.customer_id
-where summation > 5000;
-
-/* 
-second ways
-
-select    c.customer_id, round(
-            sum(
-              od.unit_price * od.quantity - (od.unit_price * od.quantity * od.discount)
-            ),
-            2
-          ) as summation
-
+select c.company_name,sum(unit_price * quantity - (unit_price * quantity * discount)) as "spent" 
 from customers c 
 inner join orders o 
-on c.customer_id = o.customer_id 
-inner join order_details od
-on od.order_id = o.order_id 
+On c.customer_id = o.customer_id
+Inner join order_details o_d
+On o.order_id = o_d.order_id
+inner join products p 
+On p.product_id = o_d.product_id
 group by c.customer_id
-having round(
-            sum(
-              od.unit_price * od.quantity - (od.unit_price * od.quantity * od.discount)
-            ),
-            2
-          ) > 5000
- */
+having sum(unit_price * quantity - (unit_price * quantity * discount)) > 5000;
